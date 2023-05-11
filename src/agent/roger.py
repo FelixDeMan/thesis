04f1239e3a39193ab1a5e9c4f68d1ddc8bd59685
nedimazar <nedimazar@gmail.com>
2023-05-11 23:50:20 +0200

    small refactor

--- src/agent/roger.py
+++ src/agent/roger.py
@@ -1,20 +1,19 @@
 import openai
+from colors import Colors
 import json
 import os
 from pathlib import Path
 import sys
 
-class Colors:
-    RESET = '\033[0m'
-    RED = '\033[31m'
-    BLUE = '\033[34m'
-    CYAN = '\033[36m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 class Roger:
     # setup = "You are roger, a MacOS terminal assistant that helps users with various tasks on the terminal. You can answer questions, provide code, and ask for specifics such as paths and system info, you can ask users to run a command and to show you the output to aid your reasoning. In cases where you must show commands, you must output all commands in an ordered JSON format containing one or more commands. Assume that you are asked for shell commands unless explicitly told to show another language. You talk in a very precise and technical manner. However, you only explain the code you output when asked for it."
     setup = "You are roger, a MacOS terminal command generator. You only reply with commands and nothing else. You only provide explanations or extra details, just the commands. Assume every requesst is for a Unix command. Ask the user when context information such as path names is missing for a command."
+
+    fewshot_prompts = [
+        {"role": "user", "content": "My operating system is MacOS Ventura"},
+        {"role": "user", "content": "I am using zsh and Oh My Zsh"},
+    ]
 
     def __init__(self, buffer_size, instance):
         self.buffer_size = buffer_size
@@ -53,11 +52,12 @@ class Roger:
         self.buffer = messages[-self.buffer_size :]
 
     def completion(self, message=None):
-        body = self.buffer
-
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "system", "content": self.setup}] + body + [message],
+            messages=[{"role": "system", "content": self.setup}]
+            + self.fewshot_prompts
+            + self.buffer
+            + [message],
         )
         content = response["choices"][0]["message"]["content"]
 
@@ -81,7 +81,6 @@ def main():
 
     roger.add_message(user_message)
     roger.add_message(assistant_message)
-
 
     # print roger in bold blue
     print(Colors.BOLD + Colors.BLUE + "roger: " + Colors.RESET, end="")
